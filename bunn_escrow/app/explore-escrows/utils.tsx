1d7a08a493400534d73f0ae9ead44a2f3a2ab8fe
Peter <obidaberechukwu@gmail.com>
2024-01-02 16:24:13 +0100

    fixed vercel error

---
+++ bunn_escrow/app/explore-escrows/utils.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useState, useEffect } from "react";
+
+export const FormatTimeLeft = ({
+  timeInSeconds,
+  className,
+}: {
+  timeInSeconds: number;
+  className?: string;
+}) => {
+  const [tick, setTick] = useState<boolean>();
+  useEffect(() => {
+    let tiker;
+    if (timeInSeconds) tiker = setInterval(() => setTick(!tick), 1000);
+    return clearInterval(tiker);
+  }, [tick, timeInSeconds]);
+
+  if (!timeInSeconds) return <span className={className}>No set time</span>;
+  const currentTime = Math.floor(Date.now() / 1000);
+  const timeDiff = timeInSeconds - currentTime;
+
+  if (timeDiff < 0) return "0h 0m 00s";
+
+  // Convert time left into days, hours, minutes, and seconds
+  const daysLeft = Math.floor(timeDiff / (60 * 60 * 24));
+  const hoursLeft = Math.floor((timeDiff % (60 * 60 * 24)) / (60 * 60));
+  const minutesLeft = Math.floor((timeDiff % (60 * 60)) / 60);
+  const secondsLeft = timeDiff % 60;
+
+  let returnString: string = `${
+    daysLeft < 0 ? daysLeft + "d" : ""
+  } ${hoursLeft}h ${minutesLeft}m ${secondsLeft}s`;
+
+  return <span className={className}>{returnString}</span>;
+};
